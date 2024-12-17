1029c78142ee3657fcefedc6c8dc52794d9a4db6
lebruntom <90044952+lebruntom@users.noreply.github.com>
2024-12-17 16:41:09 +0100

    login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,40 +1,40 @@
+<script setup>
+import { useRouter } from 'vue-router';
+
+const users = [
+    {
+        username: 'admin',
+        password: 'admin'
+    },
+    {
+        username: 'user',
+        password: 'user'
+    }
+]
+
+const router = useRouter()
+function handleLogin(e){
+    const user = {
+        username: e.target.username.value,
+        password: e.target.password.value
+    }
+    if(!users.find(u => u.username === user.username && u.password === user.password)){
+        alert('Nom d\'utilisateur ou mot de passe incorrect');
+        return;
+    }
+    localStorage.setItem('currentUser', JSON.stringify(user));
+    router.push('/projects');
+}
+
+</script>
+
 <template>
     <div class="login-form">
       <h2>Connexion</h2>
       <form @submit.prevent="handleLogin">
-        <input v-model="username" placeholder="Nom d'utilisateur" required />
-        <input type="password" v-model="password" placeholder="Mot de passe" required />
+        <input name="username" placeholder="Nom d'utilisateur" required />
+        <input name="password" placeholder="Mot de passe" required />
         <button type="submit">Se connecter</button>
       </form>
-      <button @click="handleRegister">S'inscrire</button>
     </div>
-  </template>
-  
-  <script>
-  export default {
-    data() {
-      return {
-        username: '',
-        password: ''
-      };
-    },
-    methods: {
-      handleLogin() {
-        const users = JSON.parse(localStorage.getItem('users'));
-        const user = users.find(u => u.username === this.username && u.password === this.password);
-        if (user) {
-          localStorage.setItem('currentUser', JSON.stringify(user));
-          this.$router.push('/projects');
-        } else {
-          alert('Identifiants incorrects');
-        }
-      },
-      handleRegister() {
-        const users = JSON.parse(localStorage.getItem('users'));
-        users.push({ username: this.username, password: this.password, role: ['developer'] });
-        localStorage.setItem('users', JSON.stringify(users));
-        alert('Inscription réussie');
-      }
-    }
-  };
-  </script>
+</template>
